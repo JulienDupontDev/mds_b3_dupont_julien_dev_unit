1cd1f528898d89d596981ce1b4d5f46acd0b9d37
Julien Dupont <julien.dupont2018@gmail.com>
2021-01-24 22:29:52 +0100

    remove comment lines

--- test/test.js
+++ test/test.js
@@ -1,13 +1,7 @@
 const isAnagram = require('../src/anagram');
 
-/*
-
-* Anagram Testing file
-
-*/
 
 test('isAnagram function exists', () => {
-  // Complete here
   expect(isAnagram).toBeDefined
 });
 
@@ -24,11 +18,7 @@ test('"Hello" is NOT an anagram of "Aloha"', () => {
 
 });
 
-/*
 
-* Various functions Testing file
-
-*/
 
 const functions = require('../src/variousfunctions');
 
@@ -40,8 +30,6 @@ describe('Checking Names', () => {
   test('User is Jeff', () => {
     const user = 'Jeff';
     expect(checkValue(user)).toBe('Jeff')
-    //Check if the user is Jeff
-    // Complete here
   });
 
   test('User is Karen', async () => {
@@ -58,34 +46,21 @@ test('Adds 2 + 2 to NOT equal 5', () => {
   expect(add(2, 2)).not.toEqual(5)
 });
 
-/*
 
-* Informations
 
-*/
-
-// CHECK FOR true & false VALUES
-// toBeNull matches only null
-// toBeUndefined matches only undefined
-// toBeDefined is the opposite of toBeUndefined
-// toBeTruthy matches anything that an if statement treats as true
-// toBeFalsy matches anything that an if statement treats as false
 
 test('Should be null', () => {
-  // Complete here
+  expect(null).toBeNull
 });
 
-// toBeFalsy
 test('Should be falsy', () => {
-  // Complete here
+  expect(false).toBeFalsy
 });
 
-// toEqual
 test('User should be Marc Antoine object', () => {
   expect(createUser()).toEqual({ firstName: 'Marc', lastName: 'Antoine' })
 });
 
-// Less than and greater than
 test('Should be under or equal 1000', () => {
   const load1 = 500;
   const load2 = 1100;
@@ -94,34 +69,22 @@ test('Should be under or equal 1000', () => {
 
 });
 
-// Regex
 test('There is no I in the word team', () => {
   expect('team').not.toMatch(/i/i)
 });
 
-// Arrays
 test('Admin should be in usernames', () => {
   usernames = ['john', 'karen', 'admin'];
 
   expect(usernames).toContain('admin')
-  // Complete here
 });
 
-// Working with async data
 
-// Async Await since ES7
 test('User fetched name should be Leanne Graham', async () => {
   const data = await functions.fetchUser();
   expect(data.name).toBe('Leanne Graham')
 
-  // Complete here
 });
-
-/*
-
-* ReverseString Testing file
-
-*/
 
 const reverseString = require('../src/reversestr');
 const { checkValue, fetchUser, add, createUser } = require('../src/variousfunctions');
